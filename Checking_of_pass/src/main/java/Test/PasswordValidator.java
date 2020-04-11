8ecdfd9e017d1a5b0c1e911c1b07c473c4f8ada3
Roman Lev <leva_6@mail.ru>
2020-04-11 17:12:39 +0300

    password validator with passay library

--- Checking_of_pass/src/main/java/Test/PasswordValidator.java
+++ Checking_of_pass/src/main/java/Test/PasswordValidator.java
@@ -1,16 +1,32 @@
 package Test;
 
+import org.passay.*;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
 
 
-    public static boolean checkingPass(String pass){
-        String patternString = "(?=.*[0-9])(?=.*[!@#$%^&*_-])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{4,8}";
-        Pattern pattern = Pattern.compile(patternString);
-        Matcher matcher = pattern.matcher(pass);
-        return matcher.matches();
+    public static boolean checkingPass (String pass) {
+        PasswordData data = new PasswordData ();
+        data.setPassword (pass);
+        CharacterCharacteristicsRule listOfRules = new CharacterCharacteristicsRule (4,
+                new CharacterRule(EnglishCharacterData.Digit),
+                new CharacterRule(EnglishCharacterData.LowerCase),
+                new CharacterRule(EnglishCharacterData.UpperCase),
+                new CharacterRule(EnglishCharacterData.Special)
+        );
+
+        org.passay.PasswordValidator validator = new org.passay.PasswordValidator(listOfRules, new LengthRule (8,16));
+        RuleResult result = validator.validate (data);
+        return result.isValid ();
+//        String patternString = "(?=.*[0-9])(?=.*[!@#$%^&*_-])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{4,8}";
+//        Pattern pattern = Pattern.compile(patternString);
+//        Matcher matcher = pattern.matcher(pass);
+//        return matcher.matches();
 
     }
+
+
 }
