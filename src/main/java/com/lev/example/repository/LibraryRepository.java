95fd2a7763555a54a218de6de06c384396dad1e8
Roman Lev <leva_6@mail.ru>
2020-05-18 06:22:38 +0300

    Fixing Streams

--- src/main/java/com/lev/example/repository/LibraryRepository.java
+++ src/main/java/com/lev/example/repository/LibraryRepository.java
@@ -4,7 +4,10 @@ import com.lev.example.entity.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LibraryRepository extends JpaRepository <Library, Integer> {
 
+    Optional <Library> findByIdBook(Integer IdBook);
 }
--- src/main/java/com/lev/example/services/LibraryServiceImpl.java
+++ src/main/java/com/lev/example/services/LibraryServiceImpl.java
@@ -16,16 +16,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.lev.example.repository.BooksRepository;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+
 
 
 @Service
 public class LibraryServiceImpl implements LibraryService {
+
+    static final long ONE_YEAR = 31536000000L;
+    static final int LIMIT_BOOKS = 4;
 
     @Autowired
     private BooksRepository booksRepository;
@@ -72,9 +73,9 @@ public class LibraryServiceImpl implements LibraryService {
         record.setIdBook(addNewRecordRequest.getIdReader());
         record.setDateTake(new java.sql.Date(new Date().getTime()));
 
-        int amountOfSpecificBook = libraryRepository.findById(record.getIdBook()).get().getAmountBook();
+        int amountOfSpecificBook = libraryRepository.findByIdBook(record.getIdBook()).get().getAmountBook();
         List <Record> allRecordsOfSpecificReader = recordRepository.findAllByIdReader(record.getIdReader());
-        long oneYear = 31536000000L;
+
 
 
         //Checking if there is enough such a book in the library
@@ -85,70 +86,25 @@ public class LibraryServiceImpl implements LibraryService {
        }
 
        //Checking if the reader has less than 3 books
-       if (allRecordsOfSpecificReader.size()<4) {
+       if (allRecordsOfSpecificReader.size()<LIMIT_BOOKS) {
 
        } else {
            throw new OverLimitBooksException();
        }
 
        //Checking if the reader hasn't the same book he wants to take
-//        ArrayList <Integer> idBookList = new ArrayList<>();
-//       for (Record r:allRecordsOfSpecificReader) {
-//            idBookList.add(r.getIdBook());
-//       }
-//
-//       for (Integer i : idBookList) {
-//           if (i.equals(record.getIdBook())) {
-//               System.out.println("The reader has the same book");
-//               return;
-//           }
-//       }
-
-//      if (allRecordsOfSpecificReader.stream()
-//              .map(Record::getIdBook)
-//              .anyMatch(Predicate.isEqual(record.getIdBook()))) {
-//            throw new TakeSameBookException();
-//      }
         allRecordsOfSpecificReader.stream()
                 .map(Record::getIdBook)
                 .filter(i->i.equals(record.getIdBook()))
-                .findAny().orElseThrow(throw new TakeSameBookException());
+                .findAny().orElseThrow(TakeSameBookException::new);
+
 
        //Checking if the reader hasn't overtaken book
-//        ArrayList <Date> dateTakeList = new ArrayList<>();
-//
-//        for (Record r:allRecordsOfSpecificReader) {
-//           if(r.getReturnDate()==null) {
-//               dateTakeList.add(r.getDateTake());
-//           }
-//        }
-//
-//        for(Date date : dateTakeList) {
-//           Date todayDate = new Date();
-//           Long difInMinus = todayDate.getTime() - date.getTime();
-//           if (difInMinus > oneYear) {
-//               System.out.println("The reader has overtaken book");
-//               return;
-//           }
-//        }
-
         allRecordsOfSpecificReader.stream()
                 .filter(d->d.getReturnDate()==null)
                 .map(Record::getDateTake)
                 .map(x->new Date().getTime()-x.getTime())
-                .filter(x->x>oneYear).findAny().orElseThrow(throw new OverLimitTimeUsingBookException());
-
-
-
-
-
-
-
-
-
-
-
-
+                .filter(x->x>ONE_YEAR).findAny().orElseThrow(OverLimitTimeUsingBookException::new);
 
         //Adding new Record
         recordRepository.save(record);
