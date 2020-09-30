f8f730d57c9382d95f63c4fffc7142daf215c3cb
Lev Roman <leva_6@mail.ru>
2020-09-30 03:59:16 +0300

    Transferring project to Linux v1.3

--- src/main/java/com/lev/example/controllers/Controller.java
+++ src/main/java/com/lev/example/controllers/Controller.java
@@ -1,18 +1,18 @@
-package com.lev.example.controllers;
-
-import com.lev.example.services.ValidatorService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-public class Controller {
-    @GetMapping ("/health")
-    public HttpStatus healthStatus (){
-      return HttpStatus.OK;
-    }
-
-    @PostMapping ("/checkingPassword")
-    public String post (@RequestBody String pas) {
-        return ValidatorService.checkPassword(pas);
-    }
-}
+//package com.lev.example.controllers;
+//
+//import com.lev.example.services.ValidatorService;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//public class Controller {
+//    @GetMapping ("/health")
+//    public HttpStatus healthStatus (){
+//      return HttpStatus.OK;
+//    }
+//
+//    @PostMapping ("/checkingPassword")
+//    public String post (@RequestBody String pas) {
+//        return ValidatorService.checkPassword(pas);
+//    }
+//}
