462155853c01155c75661c70c49f4aa6fd439191
Lev Roman <leva_6@mail.ru>
2020-10-05 13:52:10 +0300

    v1

--- src/main/java/com/lev/example/controllers/Controller.java
+++ src/main/java/com/lev/example/controllers/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 //asdasd
+//sadafasfasfas
 public class Controller {
     @GetMapping ("/health")
     public HttpStatus healthStatus (){
